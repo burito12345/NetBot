83ca0e990ffe9d62682e86f3e1ea5a82e5451ba1
kainzigartig <kainz356@gmail.com>
2017-11-13 13:31:29 +0100

    Merge branch 'master' of C:\Users\sebas\Desktop\4cWI1617\SWP1\beispiele\NetBot with conflicts.

--- src/main/java/Nslookup.java
+++ src/main/java/Nslookup.java
@@ -1,2 +1,34 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
 public class Nslookup {
+    private ArrayList<String> list;
+
+    public void performNSLookup(String nslookup) {
+        list = new ArrayList<String>();
+
+        System.out.println(nslookup);
+
+        try {
+
+            InetAddress inetHost = InetAddress.getByName(nslookup);
+            String hostName = inetHost.getHostName();
+            System.out.println("Die eingegebene Webseite war: " + hostName +
+                    "\nDie IP der Internetseite ist: " + inetHost.getHostAddress()
+                    + "\nDer Hostname: " + inetHost.getCanonicalHostName());
+            list.add(hostName);
+            list.add(inetHost.getHostAddress());
+            list.add(inetHost.getCanonicalHostName());
+
+
+        } catch (UnknownHostException ex) {
+
+            System.out.println("Unrecognized host");
+        }
+    }
+
+    public ArrayList<String> getInet(){ return list;}
+
+
 }
